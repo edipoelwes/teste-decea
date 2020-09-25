61b405110fc6350ef729098c5e7c456ef0bb64a9
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-09-24 21:41:27 -0300

    Autenticação com JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
   use HasFactory, Notifiable;
 
@@ -56,4 +57,24 @@ class User extends Authenticatable
 
     $this->attributes['password'] = bcrypt($value);
   }
+
+  /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
+Route::post('/auth/login', 'AuthController@login')->name('login');
+Route::post('/auth/logout', 'AuthController@logout')->name('logout');
+Route::post('/refresh', 'AuthController@refresh')->name('refresh');
+Route::post('/me', 'AuthController@me')->name('me');
 
 Route::apiResources([
   '/company' => 'CompanyController',
